d7e415bf8367b2e71ad1c06c176e11399cc148d2
Ravshan <ravkart@gmail.com>
2023-02-26 22:37:34 -0800

    updating origin url

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const server = app.listen(PORT, () => console.log(`Listening on http://localhost
 const socket = require("socket.io");
 const io = socket(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		// origin: "http://localhost:3000",
+		origin: "https://battleship-client-test.vercel.app/",
 	},
 });
 
